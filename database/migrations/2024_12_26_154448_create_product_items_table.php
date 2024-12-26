67c0da72fe792a85dc6729c4fbaf1c527a06c699
Kiryl Savitski <kirill.savickiy@yahoo.com>
2024-12-26 20:55:09 +0300

    Create Migrations

---
+++ database/migrations/2024_12_26_154448_create_product_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_items', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->integer('size')->nullable();
+            $table->string('dough_type')->nullable();
+            $table->foreignId('product_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_items');
+    }
+};
